e8e166baaa02c3c82de9cd25d09f8838fecbb91d
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-10-29 10:17:03 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # legekrogen
+
 legekrogen - fullstack project
+
+**Backend**
+npm packages installed:
+dotenv express mongoose
+
+**Frontend**
+npm packages installed:
+react-router-dom
+@mui/material @emotion/react @emotion/styled
+@mui/icons-material
+react-toastify
+react-icons
+@uidotdev/usehooks
+jwt-decode
--- sites/WWW/legekrogen/src/main.jsx
+++ sites/WWW/legekrogen/src/main.jsx
@@ -1,10 +1,66 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  palette: {
+    primary: { main: "#013e87" },
+    secondary: { main: "#2e74c9" },
+    tertiary: { main: "#426ee9ff" },
+    error: { main: "#bb1509ff" },
+    warning: { main: "#e9a615ff" },
+    info: { main: "#537df1ff" },
+    success: { main: "#18810eff" },
+    background: { primary: "#303030ff", secondary: "#fff5e4ff" },
+  },
+  typography: {
+    fontFamily: "'Poppins', sans-serif",
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
+  </StrictMode>
+);
